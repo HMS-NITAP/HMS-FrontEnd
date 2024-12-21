efaa2fcffd2c0ae4eb1702f9f68bc671c12fdf5e
swethaguntuboina <157233883+LithinAdari@users.noreply.github.com>
2024-12-21 11:38:08 +0530

    Updated iOS Build Settings

--- ios/frontend/AppDelegate.mm
+++ ios/frontend/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
-
+#import <React/RCTRootView.h>
 #import <React/RCTBundleURLProvider.h>
+#import "RNSplashScreen.h" // Import the SplashScreen header
 
 @implementation AppDelegate
 
@@ -10,8 +11,21 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:[self getBundleURL]
+                                                      moduleName:self.moduleName
+                                               initialProperties:self.initialProps
+                                                   launchOptions:launchOptions];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
+  
+  [RNSplashScreen show];  // Show the splash screen
+  return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
